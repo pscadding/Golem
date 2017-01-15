d02a97aa654bdf1d50a4db9315eb988f833942ae
Philip Scadding <poscadding@hotmail.co.uk>
2017-01-15 21:34:31 +0000

    basic functions of forwards and backwards added. tidy of code removed unneeded clean up of GPIO that was causing an issue

--- pyro_client.py
+++ pyro_client.py
@@ -2,11 +2,9 @@
 import sys
 import Pyro4
 import Pyro4.util
-Pyro4.config.HOST = "192.168.0.66"
 sys.excepthook = Pyro4.util.excepthook
-
-name = "123" # input("What is your name? ").strip()
 
 gc = Pyro4.Proxy("PYRONAME:golem.controller@192.168.0.66:9090") # get a Pyro proxy to the greeting object
 
-# print(gc.move_foward(0.01))   # call method normally
+gc.move_fowards(2)
+gc.move_backwards(2)
--- pyro_server.py
+++ pyro_server.py
@@ -3,11 +3,14 @@ import Pyro4
 import RPi.GPIO as GPIO
 import time
 
+
+
 @Pyro4.expose
 class GolemController(object):
 
     def __init__(self):
         print("in init")
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
 
@@ -18,11 +21,31 @@ class GolemController(object):
         GPIO.setup(10, GPIO.OUT)
 
         # Turn all motors off
-        GPIO.cleanup()
+        GPIO.output(7, 0)
+        GPIO.output(8, 0)
+        GPIO.output(9, 0)
+        GPIO.output(10, 0)
 
-        self.move_foward()
+    def stop(self):
+        GPIO.output(9, 0)
+        GPIO.output(10, 0)
+        GPIO.output(7, 0)
+        GPIO.output(8, 0)
 
-    def move_foward(self, period=0.5):
+    def move_backwards(self, period=0.5):
+        # Turn the right motor forwards
+        GPIO.output(9, 1)
+        GPIO.output(10, 0)
+
+        # Turn the left motor forwards
+        GPIO.output(7, 0)
+        GPIO.output(8, 1)
+
+        time.sleep(period)
+
+        self.stop()
+
+    def move_fowards(self, period=0.5):
         # Turn the right motor forwards
         GPIO.output(9, 0)
         GPIO.output(10, 1)
@@ -33,9 +56,9 @@ class GolemController(object):
 
         time.sleep(period)
 
-        GPIO.cleanup()
+        self.stop()
 
 
-Pyro4.Daemon.serveSimple({GolemController: "golem.controller"},
+Pyro4.Daemon.serveSimple({GolemController(): "golem.controller"},
                          host = '192.168.0.66',
                          ns=True)
